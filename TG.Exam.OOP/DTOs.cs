fc4f97f5f02bdb3393424fc60bf758d54e69b226
Igor Vitvinov <igor.vitvinov@fastdev.com>
2019-11-20 14:39:57 +0400

    Change OOP

--- TG.Exam.OOP/DTOs.cs
+++ TG.Exam.OOP/DTOs.cs
@@ -7,15 +7,11 @@ using System.Threading.Tasks;
 namespace TG.Exam.OOP
 {
 
-    public class Employee
+    public class Employee: Extend
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public int Salary { get; set; }
-        public string ToString2()
-        {
-           return this.ToString();
-        }
    }
 
     public class SalesManager : Employee
@@ -29,13 +25,17 @@ namespace TG.Exam.OOP
         public int Customers { get; set; }
     }
 
-    public class Dog
+    public class Dog: Extend
     {
         public string Name { get; set; }
         public int Age { get; set; }
-        public string ToString2()
-        {
-           return this.ToString();
-        }
+   }
+
+    public class Extend
+    {
+       public string ToString2()
+       {
+          return this.ToString();
+       }
    }
 }
